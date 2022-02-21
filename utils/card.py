af3e445009aa31fb25328155ee02a50cc7d74b64
IrinaSing <irinasingh.mail@yandex.ru>
2022-02-21 16:38:52 +0100

    card start

--- utils/card.py
+++ utils/card.py
@@ -1,1 +1,25 @@
 # card
+
+class Symbol:
+    """Class representing symbol (suit)"""
+
+    def __init__(self, color: str, icon: str):        
+        suits = ["♥", "♦", "♣", "♠"]
+        # check if entered suit is among existing suits
+        if (icon in suits):
+            self.icon = icon
+        else:
+            raise ValueError("Such suit does not exist")
+        self.color = color
+
+Cardesses = Symbol("red", "♥")
+        
+
+class Cards(Symbol):  # <--- Inherits from Symbol
+    """
+    A class that defines a card.
+
+    It inherits from the class symbol.
+    """
+    def __init__(self, color: str, icon: str, value: str):
+        pass
